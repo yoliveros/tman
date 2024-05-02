3f3372bd9815083a514132c93708cd48e2abea45
yoliveros <28690224+yoliveros@users.noreply.github.com>
2024-05-01 22:23:23 -0400

    feat: creating .conf

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,24 @@
 const std = @import("std");
 
+const log = std.log.scoped(.tman);
+
+const conf_file = "~/.tman.conf";
+
 pub fn main() !void {
-    std.debug.print("Hello, World!\n", .{});
+    //args
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const allocator = gpa.allocator();
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    var file = try std.fs.createFileAbsolute("/home/yoliveros/.tman.conf", .{ .read = true });
+    defer file.close();
+
+    const buf = try file.reader().readAllAlloc(allocator, 1024 * 1024);
+    defer allocator.free(buf);
+
+    log.info("Using config file at {s}\n", .{buf});
 }
