5f7394627771bf32914d1ffb74e0babdbf8b9784
yoliveros <28690224+yoliveros@users.noreply.github.com>
2024-05-13 20:45:47 -0400

    feat: folders working

--- src/DirHandler.zig
+++ src/DirHandler.zig
@@ -3,20 +3,48 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 
-pub fn openDirsAbsolute(allocator: Allocator, dirs: []const []const u8, home: []const u8) !ArrayList([]const u8) {
+pub fn openDirsAbsolute(
+    allocator: Allocator,
+    dirs: []const []const u8,
+    home: []const u8,
+) !ArrayList([]const u8) {
     var dir_list = ArrayList([]const u8).init(allocator);
     for (dirs) |dir| {
         if (std.mem.eql(u8, dir, "/")) continue;
 
-        const path = try std.fs.path.join(allocator, &[_][]const u8{ home, dir });
+        const path = try std.fs.path.join(
+            allocator,
+            &[_][]const u8{
+                home,
+                dir,
+            },
+        );
 
-        var absolute_dir = try std.fs.openDirAbsolute(path, .{ .iterate = true });
+        var absolute_dir = try std.fs.openDirAbsolute(path, .{
+            .iterate = true,
+            .access_sub_paths = false,
+        });
         defer absolute_dir.close();
 
         var walker = absolute_dir.walk(allocator) catch unreachable;
 
         while (try walker.next()) |entry| {
-            dir_list.append(try allocator.dupe(u8, entry.basename)) catch unreachable;
+            if (entry.kind != .directory) continue;
+
+            // only first level
+            if (std.mem.indexOf(u8, entry.path, "/") != null) continue;
+
+            const sub_path = try std.fs.path.join(
+                allocator,
+                &[_][]const u8{
+                    path,
+                    entry.path,
+                },
+            );
+
+            dir_list.append(
+                try allocator.dupe(u8, sub_path),
+            ) catch unreachable;
         }
     }
 
--- src/main.zig
+++ src/main.zig
@@ -11,6 +11,18 @@ const log = std.log.scoped(.tman);
 
 const conf_file = ".tman.conf";
 
+fn tmuxHandle(allocator: std.mem.Allocator, path: []const u8) void {
+    const name = std.fs.path.basename(path);
+
+    const tmux_path = std.mem.concat(allocator, u8, &[_][]const u8{ "tmux new-session -s ", name, " -c ", path }) catch unreachable;
+
+    std.os.linux.execve(
+        @ptrCast(path),
+        @alignCast(tmux_path),
+        &[_][]const u8{},
+    );
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -42,7 +54,7 @@ pub fn main() !void {
     var text_input = TextInput.init(allocator, &vx.unicode);
     defer text_input.deinit();
 
-    var selected_dir: ?usize = null;
+    var selected_dir: usize = 0;
 
     var dirs = std.ArrayList([]const u8).init(allocator);
 
@@ -57,31 +69,55 @@ pub fn main() !void {
 
         switch (event) {
             .key_press => |key| {
-                if (key.codepoint == 'c' and key.mods.ctrl) {
+                if (key.matches(vaxis.Key.enter, .{})) {
+                    tmuxHandle(allocator, dirs.items[selected_dir]);
+                } else if (key.matches(vaxis.Key.down, .{}) and
+                    selected_dir < dirs.items.len - 1)
+                {
+                    selected_dir += 1;
+                } else if (key.matches(vaxis.Key.up, .{}) and
+                    selected_dir > 0)
+                {
+                    selected_dir -= 1;
+                } else if (key.codepoint == 'c' and key.mods.ctrl or
+                    key.matches(vaxis.Key.escape, .{}))
+                {
                     break;
-                } else if (key.matches(vaxis.Key.tab, .{})) {
-                    if (selected_dir == null) {
-                        selected_dir = 0;
-                    } else {
-                        selected_dir.? = @min(dirs.items.len - 1, selected_dir.? + 1);
-                    }
-                } else if (key.matches(vaxis.Key.tab, .{ .shift = true })) {
-                    if (selected_dir == null) {
-                        selected_dir = 0;
-                    } else {
-                        selected_dir.? = selected_dir.? -| 1;
-                    }
-                } else if (key.matches(vaxis.Key.enter, .{})) {
-                    if (selected_dir) |i| {
-                        // log.err("enter", .{});
-                        try text_input.insertSliceAtCursor(dirs.items[i]);
-                        selected_dir = null;
-                    }
-                } else {
-                    if (selected_dir == null)
-                        try text_input.update(.{ .key_press = key });
                 }
             },
+            // if (key.codepoint == 'c' and key.mods.ctrl or
+            //     key.matches(vaxis.Key.escape, .{}))
+            // {
+            //     break;
+            // } else if (key.matches(vaxis.Key.tab, .{}) or
+            //     key.matches(vaxis.Key.down, .{}))
+            // {
+            //     if (selected_dir == null) {
+            //         selected_dir = 0;
+            //     } else {
+            //         selected_dir.? = @min(
+            //             dirs.items.len - 1,
+            //             selected_dir.? + 1,
+            //         );
+            //     }
+            // } else if (key.matches(vaxis.Key.tab, .{ .shift = true }) or
+            //     key.matches(vaxis.Key.up, .{ .shift = true }))
+            // {
+            //     if (selected_dir == null) {
+            //         selected_dir = 0;
+            //     } else {
+            //         selected_dir.? = selected_dir.? -| 1;
+            //     }
+            // } else if (key.matches(vaxis.Key.enter, .{})) {
+            //     if (selected_dir) |i| {
+            //         // log.err("enter", .{});
+            //         try text_input.insertSliceAtCursor(dirs.items[i]);
+            //         selected_dir = null;
+            //     }
+            // } else {
+            //     if (selected_dir == null)
+            //         try text_input.update(.{ .key_press = key });
+            // }
             .winsize => |ws| {
                 try vx.resize(allocator, ws);
             },
@@ -93,16 +129,13 @@ pub fn main() !void {
 
         text_input.draw(win);
 
-        if (selected_dir) |i| {
-            win.hideCursor();
-            for (dirs.items, 0..) |opt, j| {
-                // log.err("i = {d}, j = {d}, opt = {s}", .{ i, j, opt });
-                var seg = [_]vaxis.Segment{.{
-                    .text = opt,
-                    .style = if (j == i) .{ .reverse = true } else .{},
-                }};
-                _ = try win.print(&seg, .{ .row_offset = j + 1 });
-            }
+        win.hideCursor();
+        for (dirs.items, 0..) |opt, j| {
+            var seg = [_]vaxis.Segment{.{
+                .text = opt,
+                .style = if (j == selected_dir) .{ .reverse = true } else .{},
+            }};
+            _ = try win.print(&seg, .{ .row_offset = j + 1 });
         }
 
         try vx.render();
